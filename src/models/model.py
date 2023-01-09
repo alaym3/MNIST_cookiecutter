2b3e26e0469ebf904fb015e2f970e9e9573f774c
alaym3 <aam5xa@virginia.edu>
2023-01-09 15:35:37 +0100

    + black

--- src/models/model.py
+++ src/models/model.py
@@ -33,9 +33,9 @@ class MyAwesomeModel(nn.Module):
 
         """
         if x.ndim != 4:
-            raise ValueError('Expected input to a 4D tensor')
+            raise ValueError("Expected input to a 4D tensor")
         if x.shape[1] != 1 or x.shape[2] != 28 or x.shape[3] != 28:
-            raise ValueError('Expected each sample to have shape [1, 28, 28]')
+            raise ValueError("Expected each sample to have shape [1, 28, 28]")
         return self.classifier(self.backbone(x))
 
 
